819ee5e4445765813d32b6dde9a966b94208cc21
Vladislav Brusokas <brusvlad4002@yandex.ru>
2025-10-12 19:43:48 +0100

    Added basic image loading and display - Images loaded using stb_image.h - Basic darkness value extrtaction - Basic grid printing

--- tiv.cpp
+++ tiv.cpp
@@ -1,9 +1,22 @@
 #include <ncurses/ncurses.h>
+#include <stdint.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 
 int main(){
     initscr();
-    printw("Hello World");
+    int width, height, bpp;
+    uint8_t* rgb_image = stbi_load("images/image.png", &width, &height, &bpp, 1);
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            printw("%d", rgb_image[i*width + j]);
+        }
+        printw("\n");
+    }
     refresh();
     getch();
+    stbi_image_free(rgb_image);
     endwin();
 }
