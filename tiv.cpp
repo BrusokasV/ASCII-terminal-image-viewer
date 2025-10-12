da26b7b3a5b5134cbbe8d48d39a941e165a00108
Vladislav Brusokas <brusvlad4002@yandex.ru>
2025-10-12 20:33:27 +0100

    Added ascii-conversion - A basic colour ramp of 10 symbols least to most bright - Brightness of a pixel is scaled to the colour ramp - Todo: add scaling of images

--- tiv.cpp
+++ tiv.cpp
@@ -1,17 +1,20 @@
 #include <ncurses/ncurses.h>
 #include <stdint.h>
+#include <cmath>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+const char map[10] = {' ','.', ':', '-', '=', '+', '*', '#', '%', '@'};
 
 int main(){
     initscr();
-    int width, height, bpp;
-    uint8_t* rgb_image = stbi_load("images/image.png", &width, &height, &bpp, 1);
+    int width, height, bpp, mapped;
+    uint8_t* rgb_image = stbi_load("images/arasaka.png", &width, &height, &bpp, 1);
     for (int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
-            printw("%d", rgb_image[i*width + j]);
+            float f = std::lerp(0, 9, (rgb_image[i*width + j] / 255));
+            printw("%c ", map[(int)(std::floor(rgb_image[i*width + j] / 25.6))]);
         }
         printw("\n");
     }
