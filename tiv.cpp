1e38c636498ad120e8b48cdbe9b21af50dee9c60
Vladislav Brusokas <brusvlad4002@yandex.ru>
2025-10-13 01:01:12 +0100

    Enhanced UI - Added borders at the top and at the bottom - Name of the file is displayed on the bottom border - Centered the image - Filename can be passed in - Moved drawing UI and Image into separate functions

--- tiv.cpp
+++ tiv.cpp
@@ -5,21 +5,54 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+void drawUI(char* filename);
+void drawImage(uint8_t* image, int width, int height, int bpp);
+
 const char map[10] = {' ','.', ':', '-', '=', '+', '*', '#', '%', '@'};
 
-int main(){
-    initscr();
-    int width, height, bpp, mapped;
-    uint8_t* rgb_image = stbi_load("images/arasaka.png", &width, &height, &bpp, 1);
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < width; j++){
-            float f = std::lerp(0, 9, (rgb_image[i*width + j] / 255));
-            printw("%c ", map[(int)(std::floor(rgb_image[i*width + j] / 25.6))]);
-        }
-        printw("\n");
+int main(int argc, char *argv[]){
+    if (argc != 2){
+        printf("Incorrect number of arguments.");
+        return 0;
     }
+    char* filename = argv[1];
+
+    int width, height, bpp;
+    int ch;
+    uint8_t* rgb_image = stbi_load(filename, &width, &height, &bpp, 1);
+
+    initscr();
+    cbreak();
+    keypad(stdscr, TRUE);
+
+    drawUI(filename);
+    drawImage(rgb_image, width, height, bpp);
     refresh();
+
     getch();
     stbi_image_free(rgb_image);
     endwin();
+}
+
+void drawUI(char* filename){
+    int len = strlen(filename) + 15;
+
+    for (int i = 0; i < COLS; i++) {
+        mvprintw(0, i, "=");
+        mvprintw(LINES-1, i, "=");
+    }
+
+    mvprintw(LINES-1, COLS/2 - len/2, "{Displaying: %s}", filename);
+}
+
+void drawImage(uint8_t* image, int width, int height, int bpp){
+    int startx = COLS/2 - width;
+    int starty = LINES/2 - height/2;
+    for (int i = 0; i < height; i++){
+        move(starty, startx);
+        for (int j = 0; j < width; j++){
+            printw("%c ", map[(int)(std::floor(image[i*width + j] / 25.6))]);
+        }
+        starty++;
+    }
 }
