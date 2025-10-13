6221639de841312b9ff61cefddae512349234edd
Vladislav Brusokas <brusvlad4002@yandex.ru>
2025-10-13 12:55:24 +0100

    Created downscale function - Called right after loading an image - Checks whether the image would fit into the existing terminal - If not, calculates a factor for downscaling and creates a downscaled version of the image - Downscaling is performed by taking the average brightness of the sector that gets downscaled to a single pixel

--- tiv.cpp
+++ tiv.cpp
@@ -6,9 +6,11 @@
 #include <stb_image.h>
 
 void drawUI(char* filename);
-void drawImage(uint8_t* image, int width, int height, int bpp);
+void drawImage(uint8_t* image, int width, int height);
+uint8_t* downscale(uint8_t* init_image, int init_width, int init_height, int* pnew_width, int* pnew_height);
 
 const char map[10] = {' ','.', ':', '-', '=', '+', '*', '#', '%', '@'};
+bool ds = false;
 
 int main(int argc, char *argv[]){
     if (argc != 2){
@@ -17,20 +19,22 @@ int main(int argc, char *argv[]){
     }
     char* filename = argv[1];
 
-    int width, height, bpp;
+    int width, height, bpp, ds_width, ds_height;
     int ch;
     uint8_t* rgb_image = stbi_load(filename, &width, &height, &bpp, 1);
+    uint8_t* ds_image = downscale(rgb_image, width, height, &ds_width, &ds_height);
 
     initscr();
     cbreak();
     keypad(stdscr, TRUE);
-
+    
     drawUI(filename);
-    drawImage(rgb_image, width, height, bpp);
+    drawImage(ds_image, ds_width, ds_height);
     refresh();
 
     getch();
     stbi_image_free(rgb_image);
+    stbi_image_free(ds_image);
     endwin();
 }
 
@@ -45,7 +49,7 @@ void drawUI(char* filename){
     mvprintw(LINES-1, COLS/2 - len/2, "{Displaying: %s}", filename);
 }
 
-void drawImage(uint8_t* image, int width, int height, int bpp){
+void drawImage(uint8_t* image, int width, int height){
     int startx = COLS/2 - width;
     int starty = LINES/2 - height/2;
     for (int i = 0; i < height; i++){
@@ -55,4 +59,44 @@ void drawImage(uint8_t* image, int width, int height, int bpp){
         }
         starty++;
     }
+}
+
+uint8_t* downscale(uint8_t* init_image, int init_width, int init_height, int* pnew_width, int* pnew_height){
+    
+    int factor = std::ceil((float)std::max(init_height, init_width) / std::min(LINES-2, COLS));
+    int new_height = init_height / factor;
+    int new_width = init_width / factor;
+    if ((init_width <= COLS) && (init_height <= LINES-2)){
+        new_height = init_height;
+        new_width = init_width;
+    }
+    uint8_t* target_image = new uint8_t[new_height*new_width];
+    int index, acc, count;
+    if ((init_width == new_width) && (init_height == new_height)){
+        for (int target_i = 0; target_i < new_height; target_i++){
+            for (int target_j = 0; target_j < new_width; target_j++){
+                index = target_i*new_width + target_j;
+                target_image[index] = init_image[index];
+            }
+        }
+    }
+    else {
+        for (int target_i = 0; target_i < new_height; target_i++){
+            for (int target_j = 0; target_j < new_width; target_j++){
+                acc = 0;
+                count = 0;
+                for (int init_i = target_i * factor; init_i < (target_i + 1) * factor; init_i++){
+                    for (int init_j = target_j * factor; init_j < (target_j + 1) * factor; init_j++){
+                        count++;
+                        acc = acc + init_image[init_i*init_width + init_j];
+                    }
+                }
+                target_image[target_i*new_width + target_j] = acc/count;
+            }
+        }
+        ds = true;
+    }
+    *pnew_height = new_height;
+    *pnew_width = new_width;
+    return target_image;
 }
