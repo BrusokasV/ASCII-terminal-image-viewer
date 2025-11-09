6bec3a0caab4aca5a8210cbc13e5c1a185e99111
Vladislav Brusokas <brusvlad4002@gmail.com>
2025-11-09 02:08:46 +0000

    Added functionality to view directories - program takes directory as an argument - reads the directory and retrieves all files inside - arrow keys can be used to navigate and select the correct file - enter used to display selected file - 'c' and 'i' used to toggle contrast and inverse mode on/off respectively - added colour

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 This is project is able to display png images as ASCII art in the terminal.
 
 ## Current functionality:
-- Reads in a png file
+- Reads in a directory and displays its contents
+- When a file is selected, reads in a png file
 - Downscales the image in parallel to fit within the terminal
-- Applies a contrast operation
 - Displays the image as ASCII art
 
 ## ToDo:
-- Implement viewing directories, not just single files
 - Implement viewing gifs/videos, other image formats
-- Add colour?
 
 ## Usage:
 Dependencies:
@@ -21,7 +19,8 @@ Compilation:
 - g++ -fopenmp tiv.cpp -o tiv -lncursesw -std=c++20
 
 Operation:
-- ./tiv [-c] \<filename\>
+- ./tiv \<directory/>
 
-Optional flags:
-- -c: disable contrast mask
+- use arrow up, arrow down to navigate the directory, enter to open a file
+- use 'c' and 'i' to toggle contrast and inverse of the image, respectively
+- use escape to close the viewer
--- tiv.cpp
+++ tiv.cpp
@@ -4,59 +4,161 @@
 #include <algorithm>
 #include <iostream>
 #include <unistd.h>
+#include <filesystem>
+#include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-void drawUI(char* filename);
+void drawUI(const char* path, const char* filename, bool displaying);
+void drawDirectory(std::vector<std::string> directory, int highlight);
 void drawImage(uint8_t* image, int width, int height);
 uint8_t* downscale(uint8_t* init_image, int init_width, int init_height, int* pnew_width, int* pnew_height);
 
-const char map[10] = {' ','.', ':', '-', '=', '+', '*', '#', '%', '@'};
-bool contrast = true;
+char map[10] = {' ','.',':','-','=','+','*','#','%','@'};
+bool mode_contrast = true;
+bool mode_inverse = false;
 
 int main(int argc, char *argv[]){
     int width, height, bpp, ds_width, ds_height;
     int ch, opt;
 
-    while ((opt = getopt(argc, argv, "c")) != -1){
+    while ((opt = getopt(argc, argv, "")) != -1){
         switch (opt){
-            case 'c':
-                contrast = false;
-                break;
             default:
-                std::cerr << "Usage: " << argv[0] << " [-c] filename \n";
+                std::cerr << "Usage: " << argv[0] << " dir/ \n";
                 return 1;
         }
     }
-    char* filename = argv[optind];
+    char* path = argv[optind];
+
+    std::vector<std::string> directory;
+
+    for (const auto &entry : std::filesystem::directory_iterator(path)) {
+        if (std::filesystem::is_regular_file(entry.status())) {
+            directory.push_back(entry.path().string());
+        }
+    }
     
     initscr();
     cbreak();
+    noecho();
     keypad(stdscr, TRUE);
-    
-    uint8_t* rgb_image = stbi_load(filename, &width, &height, &bpp, 1);
-    uint8_t* ds_image = downscale(rgb_image, width, height, &ds_width, &ds_height);
-    
-    drawUI(filename);
-    drawImage(ds_image, ds_width, ds_height);
-    refresh();
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
 
-    getch();
+    int highlight = 1;
+    bool displaying = false;
+    bool shouldend = false;
+    const char* filename;
+
+    uint8_t* rgb_image;
+    uint8_t* ds_image;
+
+    attron(COLOR_PAIR(1));
+
+    drawDirectory(directory, highlight);
+    drawUI(path, filename, displaying);
+
+    while (!shouldend) {
+        ch = getch();
+        switch (ch) {
+            case KEY_UP:
+                highlight--;
+                if (highlight <= 0) highlight = directory.size();
+                drawDirectory(directory, highlight);
+                drawUI(path, filename, displaying);
+                break;
+            case KEY_DOWN:
+                highlight++;
+                if (highlight > (int)directory.size()) highlight = 1;
+                drawDirectory(directory, highlight);
+                drawUI(path, filename, displaying);
+                break;
+            case 10:
+            case KEY_ENTER:
+                clear();
+                mvprintw(LINES/2, COLS/2 - 5, "Loading...");
+                displaying = true;
+                filename = directory[highlight-1].c_str();
+                rgb_image = stbi_load(filename, &width, &height, &bpp, 1);
+                ds_image = downscale(rgb_image, width, height, &ds_width, &ds_height);
+                drawImage(ds_image, ds_width, ds_height);
+                drawDirectory(directory, highlight);
+                drawUI(path, filename, displaying);
+                break;
+            case 105:
+            case 73:
+                mode_inverse = !mode_inverse;
+                std::reverse(std::begin(map), std::end(map));
+                if (displaying){
+                    drawImage(ds_image, ds_width, ds_height);
+                    drawDirectory(directory, highlight);
+                }
+                drawUI(path, filename, displaying);
+                break;
+            case 99:
+            case 67:
+                mode_contrast = !mode_contrast;
+                if (displaying){
+                    drawImage(ds_image, ds_width, ds_height);
+                    drawDirectory(directory, highlight);
+                }
+                drawUI(path, filename, displaying);
+                break;
+            case 27:
+                shouldend = true;
+                break;
+            default:
+                break;
+        }
+    }
     stbi_image_free(rgb_image);
     stbi_image_free(ds_image);
+
+    attroff(COLOR_PAIR(1));
+    
     endwin();
 }
 
-void drawUI(char* filename){
-    int len = strlen(filename) + 15;
+void drawUI(const char* path, const char* filename, bool displaying){
+    int len = 0;
+    if (displaying) len = strlen(filename) + 3;
+    else len = strlen(path) + 3;
 
     for (int i = 0; i < COLS; i++) {
         mvprintw(0, i, "=");
         mvprintw(LINES-1, i, "=");
     }
 
-    mvprintw(LINES-1, COLS/2 - len/2, "{Displaying: %s}", filename);
+    const char* c_state;
+    const char* i_state;
+
+    if (mode_contrast) c_state = "ON ";
+    else c_state = "OFF";
+
+    if (mode_inverse) i_state = "ON ";
+    else i_state = "OFF";
+
+    mvprintw(2, COLS - 36, "MODE:");
+    mvprintw(3, COLS - 36, "Contrast: %s (press 'c' to toggle)", c_state);
+    mvprintw(4, COLS - 36, "Inverse:  %s (press 'i' to toggle)", i_state);
+
+    if (displaying) mvprintw(LINES-1, COLS/2 - len/2, "{/%s}", filename);
+    else mvprintw(LINES-1, COLS/2 - len/2, "{/%s}", path);
+}
+
+void drawDirectory(std::vector<std::string> directory, int highlight){
+    int starty = 2;
+
+    for (auto name : directory){
+        if (starty / 2 == highlight) attron(A_REVERSE);
+        mvprintw(starty, 2, "[%s]", name.c_str());
+        if (starty / 2 == highlight) attroff(A_REVERSE);
+        starty+=2;
+    }
+
+    refresh();
 }
 
 void drawImage(uint8_t* image, int width, int height){
@@ -77,12 +179,13 @@ void drawImage(uint8_t* image, int width, int height){
         move(starty, startx);
         for (int j = 0; j < width; j++){
             int val = 0;
-            if (contrast) val = (int)(std::floor((image[i*width + j] - v_min) * a_scale / (v_max - v_min)));
+            if (mode_contrast) val = (int)(std::floor((image[i*width + j] - v_min) * a_scale / (v_max - v_min)));
             else val = (int)(std::floor(image[i*width + j] / b_factor));
             printw("%c ", map[val]);
         }
         starty++;
     }
+    refresh();
 }
 
 uint8_t* downscale(uint8_t* init_image, int init_width, int init_height, int* pnew_width, int* pnew_height){
